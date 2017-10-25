c7221037b3b986442d7e47fea1c4a3e0ef65d258
Aidan Clemente <aidanclemente@gmail.com>
2017-10-25 01:09:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var recipes2 = Object.assign({}, object);
-  delete recipes2.key;
+  delete recipes2[key];
   return recipes2;
 }
