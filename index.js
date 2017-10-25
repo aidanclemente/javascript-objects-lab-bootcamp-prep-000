c5fd94ef562de3ab5fec6ae167553afdcfecc76c
Aidan Clemente <aidanclemente@gmail.com>
2017-10-25 01:01:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  recipes1 = Object.assign({}, object, { [key]: value });
+  return recipes1;
+}
+
+// function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+//    object[key] = value
+//    return object
+// }
